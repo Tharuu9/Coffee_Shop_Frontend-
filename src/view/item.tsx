3ff2c9841230a5557f1b6f7e9a2f2a61e151abe5
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2024-06-27 00:54:28 +0530

    item updated

--- src/view/item.tsx
+++ src/view/item.tsx
@@ -1,9 +1,11 @@
 import ItemCard from "../components/card/itemCard.tsx";
-import {createRef, useEffect, useState} from "react";
+import React, {createRef, useEffect, useState} from "react";
 import AddCoffee from "../components/layout/add/add.coffee.tsx";
 import AddDessert from "../components/layout/add/add.dessert.tsx";
 import axios from "axios";
 import EmptyItem from "../components/component/empty/empty.item.tsx";
+import {toast, ToastContainer} from "react-toastify";
+import * as ToastUtil from "../util/toastUtil.tsx";
 
 interface CoffeeData {
     _id: string;
@@ -20,7 +22,7 @@ interface DessertData {
     name: string;
     desc: string;
     size: number;
-    price:number;
+    price: number;
     qty: number;
     image: string;
 }
@@ -34,10 +36,11 @@ const Item = (): JSX.Element => {
     const [dessertData, setDessertData] = useState<DessertData[]>([]);
     const coffeeRef = createRef();
     const dessertRef = createRef();
+    const toastId = React.useRef<any>(null);
 
     const setCardDataToRef = (data: CoffeeData | DessertData, cardType: string) => {
 
-        if (cardType === "coffee"){
+        if (cardType === "coffee") {
             // @ts-ignore
             coffeeRef?.current?.setCoffee(data);
             return;
@@ -98,33 +101,37 @@ const Item = (): JSX.Element => {
         }
     }
 
+    const showNotify = (title: string, message: string) => {
+        toast.dismiss(toastId.current);
+        toastId.current = ToastUtil.error(title, message);
+    }
+
     return (
         <section className={'w-full h-full bg-white flex'}>
+            <ToastContainer toastClassName={"toast-class"} bodyClassName={"toast-body"}/>
             <div className={'w-[78%] h-full'}>
                 <div className={'pl-10 py-4 font-Index tracking-wider'}>
                     <h1 className={'text-2xl font-bold text-[#3c3c3c]'}>Items</h1>
                     <h4 className={'text-[12px] text-gray-400'}>Good morning kasun. You have added new 4 items .</h4>
                 </div>
-                <div className={'w-full h-8 flex px-10 gap-2'}>
-                    <button
-                        onClick={changeButton}
-                        type={'button'}
-                        className={`w-20 h-10 cursor-pointer text-[13px] ${coffeeState ? 'bg-[#3C3C3C] text-white rounded-xl' : 'border-[1px] border-gray-300 rounded-[25px]'} 
-                         font-round hover:rounded-xl transition-all ease-linear duration-200`}>Coffee
+                <div
+                    className={'mx-10 border-[1px] w-fit border-gray-300 bg-gray-100 rounded-lg text-gray-500 text-[13px] font-cde'}>
+                    <button onClick={changeButton}
+                            type={'button'} className={`py-2 w-[70px] border-r-[1px] border-gray-300 transition-all ease-linear duration-200 delay-100
+                            ${coffeeState && 'w-[85px] text-white bg-[#2c2c2c] rounded-lg shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]'}`}>Coffee
                     </button>
-                    <button
-                        onClick={changeButton}
-                        type={'button'}
-                        className={`w-20 h-10 cursor-pointer text-[13px] ${dessertState ? 'bg-[#3C3C3C] text-white rounded-xl' : 'border-[1px] border-gray-300 rounded-[25px]'} 
-                         font-round hover:rounded-xl transition-all ease-linear duration-200`}>Dessert
+                    <button onClick={changeButton}
+                            type={'button'} className={`py-2 w-[70px] transition-all ease-linear duration-200 delay-100
+                            ${dessertState && 'w-[85px] text-white bg-[#2c2c2c] rounded-lg shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]'}`}>Dessert
                     </button>
                 </div>
                 <div
                     className={'w-full h-[80vh] flex flex-wrap mt-4 overflow-y-scroll px-10 pt-3 pb-12 bg-[#f6f6f6] border-t-[1px] border-gray-200'}>
                     {
                         coffeeState ?
-                                coffeeData.length > 0 ? coffeeData.map(value => {
+                            coffeeData.length > 0 ? coffeeData.map(value => {
                                     return <ItemCard
+                                        showTosty={showNotify}
                                         cardType={"coffee"}
                                         onLoadAction={fetchData}
                                         setCardData={setCardDataToRef}
@@ -137,35 +144,41 @@ const Item = (): JSX.Element => {
                                         desc={value.desc}
                                         image={`http://localhost:8080/images/${value.image}`}/>
                                 })
-                                    :
-                                    <EmptyItem/>
-
+                                :
+                                <EmptyItem/>
                             :
 
-                                dessertData.length > 0 ? dessertData.map(value => {
-                                return <ItemCard
-                                    cardType={"dessert"}
-                                    onLoadAction={fetchData}
-                                    setCardData={setCardDataToRef}
-                                    qty={value.qty}
-                                    key={value._id}
-                                    size={value.size}
-                                    price={value.price}
-                                    name={value.name}
-                                    _id={value._id}
-                                    desc={value.desc}
-                                    image={`http://localhost:8080/images/${value.image}`}/>
-                            })
+                            dessertData.length > 0 ? dessertData.map(value => {
+                                    return <ItemCard
+                                        cardType={"dessert"}
+                                        onLoadAction={fetchData}
+                                        setCardData={setCardDataToRef}
+                                        qty={value.qty}
+                                        key={value._id}
+                                        size={value.size}
+                                        price={value.price}
+                                        name={value.name}
+                                        _id={value._id}
+                                        desc={value.desc}
+                                        showTosty={showNotify}
+                                        image={`http://localhost:8080/images/${value.image}`}/>
+                                })
                                 :
                                 <EmptyItem/>
                     }
                 </div>
             </div>
 
-            <div className={'w-[22%] h-full border-l-2 border-gray-200 bg-white px-4 pt-20'}>
+            <div className={'w-[22%] h-full border-l-2 border-gray-200 bg-white px-4 py-12'}>
                 {
-                    coffeeState ? <AddCoffee ref={coffeeRef} onLoadAction={fetchData} onSetCoffee={setCoffee}/> :
-                        <AddDessert ref={dessertRef} onLoadAction={fetchData} onSetDessert={setDessert}/>
+                    coffeeState ? <AddCoffee ref={coffeeRef}
+                                             onLoadAction={fetchData}
+                                             onSetCoffee={setCoffee}
+                                             showTosty={showNotify}/> :
+                        <AddDessert ref={dessertRef}
+                                    onLoadAction={fetchData}
+                                    onSetDessert={setDessert}
+                                    showTosty={showNotify}/>
                 }
             </div>
 
